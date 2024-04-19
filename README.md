aabdee5e3c43ee1fd4990eab0f1a656232e20540
yinuotxie <yinuox@uw.edu>
2024-04-19 05:24:42 -0400

    update README

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# review-gpt
-UPenn CIS6200 Advanced Topic in Deep Learning Final Project
+# Machine Learning Paper Reviews GPT
+![pipeline](assets/pipeline.png)
+
+## Description
+This project, developed as a final project for the [UPenn CIS6200 Advanced Topics in Deep Learning](https://docs.google.com/document/d/1dkQ4XRhaiZFjGu5i_8Qcoi6MkHwOfivmFFWhBrBF30I/edit), aims to investigate the use of large language models (LLMs) for generating machine learning paper reviews. It is inspired by the study ["Can large language models provide useful feedback on research papers? A large-scale empirical analysis"](https://arxiv.org/pdf/2310.01783.pdf) and utilizes similar techniques. For more details, please see our [project report](report.pdf).
+
+## Methods
+We used models such as GPT-3.5-turbo and GPT-4-turbo, and fine-tuned [Mistral-7B-Instruct-v0.2](https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2) on our custom dataset.
+
+### Dataset
+Our dataset, sourced from [OpenReview](https://openreview.net/), includes papers and their reviews. We also used GPT-4 to generate summary reviews, which aided in the fine-tuning process. The dataset is published on the Huggingface dataset hub and can be accessed [here](travis0103/abstract_paper_review).
+
+### Fine-tuning
+The models were fine-tuned using the `train.py` script. Our fine-tuned model is available on the Huggingface model hub, accessible [here](travis0103/mistral_7b_paper_review_lora).
+
+## Usage 
+The project provides two pipelines for generating reviews:
+
+### Installation
+Clone the repository and set up the environment:
+```bash
+git clone git@github.com:yinuotxie/MLPapersReviewGPT.git
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+Note: The scipdf_parser package, required for PDF text extraction, must run within a Docker container. Instructions are available in the [scipdf_parser repository](https://github.com/titipata/scipdf_parser).
+
+### Model Pipeline
+Generate reviews using the fine-tuned model. Currently, only the abstracts of papers are supported:
+```bash
+python model_review.py 
+    --pdf_file <path_to_pdf_file> 
+    --device <device> 
+    --model_id <model_id> 
+    --quantize
+```
+
+### GPT Pipeline
+Alternatively, use the GPT pipeline to generate reviews:
+```bash
+python gpt_review.py 
+    --pdf_file <path_to_pdf_file> 
+    --openai_api_key <your_openai_api_key> 
+    --model <gpt-3.5-turbo or gpt-4-turbo> 
+    --method <full or abstract> 
+    --one_shot
+```
+
+## Acknowledgements
+We extend our deepest gratitude to our professor, [Prof. Lyle Ungar](https://www.cis.upenn.edu/~ungar/), for his invaluable guidance and support throughout the project. We also thank the teaching assistants, Visweswaran Baskaran, Haotong (Victor) Tian, and Royina Karegoudra Jayanth, for their helpful feedback and assistance. 
+
+## References
+* [Can large language models provide useful feedback on research papers? A large-scale empirical analysis](https://arxiv.org/pdf/2310.01783.pdf)
+* [scipdf_parser](https://github.com/titipata/scipdf_parser)
+* [Mistral-7B-Instruct-v0.2](https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2)
--- model_review.py
+++ model_review.py
@@ -14,6 +14,7 @@ from prompts import SYSTEM_PROMPT
 from pdf_parser import parse_pdf_abstract, generate_user_input
 from utils import setup_logger
 
+
 def load_model(model_id: str, quantize: bool, device: str) -> tuple:
     """
     Loads a specified model from Hugging Face with optional quantization.
@@ -26,25 +27,25 @@ def load_model(model_id: str, quantize: bool, device: str) -> tuple:
     Returns:
         tuple: Loaded model and tokenizer from Hugging Face.
     """
-    if quantize: 
+    if quantize:
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=quantize,
             bnb_4bit_use_double_quant=quantize,
             bnb_4bit_quant_type="nf4" if quantize else None,
-            bnb_4bit_compute_dtype=torch.bfloat16 if quantize else None
+            bnb_4bit_compute_dtype=torch.bfloat16 if quantize else None,
         )
 
         model = AutoPeftModelForCausalLM.from_pretrained(
             model_id,
             torch_dtype=torch.bfloat16,
             quantize_config=bnb_config,
-            device_map=device
+            device_map=device,
         )
     else:
         model = AutoPeftModelForCausalLM.from_pretrained(model_id, device_map=device)
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    
+
     return model, tokenizer
 
 
@@ -58,15 +59,22 @@ def extract_sections(text: str) -> str:
     Returns:
         str: Nicely formatted string containing the extracted sections.
     """
-    pattern = r"\[(Significance and novelty|Potential reasons for acceptance|Potential reasons for rejection|Suggestions for improvement)\]" \
-            r"(.*?)(?=\n\[(Significance and novelty|Potential reasons for acceptance|Potential reasons for rejection|Suggestions for improvement)\]|\Z)"
-    
+    pattern = (
+        r"\[(Significance and novelty|Potential reasons for acceptance|Potential reasons for rejection|Suggestions for improvement)\]"
+        r"(.*?)(?=\n\[(Significance and novelty|Potential reasons for acceptance|Potential reasons for rejection|Suggestions for improvement)\]|\Z)"
+    )
+
     sections = re.findall(pattern, text, flags=re.DOTALL)
     section_dict = {section[0]: section[1].strip() for section in sections}
     return "\n\n".join(f"[{key}]\n{value}" for key, value in section_dict.items())
 
 
-def inference(user_input: str, model: AutoPeftModelForCausalLM, tokenizer: AutoTokenizer, device: str) -> str:
+def inference(
+    user_input: str,
+    model: AutoPeftModelForCausalLM,
+    tokenizer: AutoTokenizer,
+    device: str,
+) -> str:
     """
     Generates a review based on user input using a pre-loaded model and tokenizer.
 
@@ -84,8 +92,16 @@ def inference(user_input: str, model: AutoPeftModelForCausalLM, tokenizer: AutoT
             {"role": "user", "content": SYSTEM_PROMPT + "\n\n" + user_input},
         ]
 
-        encoded_input = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
-        generated_ids = model.generate(encoded_input, max_new_tokens=1024, do_sample=True, pad_token_id=tokenizer.eos_token_id, eos_token_id=tokenizer.eos_token_id)
+        encoded_input = tokenizer.apply_chat_template(messages, return_tensors="pt").to(
+            device
+        )
+        generated_ids = model.generate(
+            encoded_input,
+            max_new_tokens=1024,
+            do_sample=True,
+            pad_token_id=tokenizer.eos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
         decoded_output = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
         return extract_sections(decoded_output)
     except Exception as e:
@@ -93,17 +109,39 @@ def inference(user_input: str, model: AutoPeftModelForCausalLM, tokenizer: AutoT
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate structured academic paper reviews using a trained language model.")
-    parser.add_argument("--pdf_file", type=str, required=True, help="Path to the PDF file.")
-    parser.add_argument("--model_id", type=str, default="travis0103/mistral_7b_paper_review_lora", help="Hugging Face model identifier.")
-    parser.add_argument("--quantize", action="store_true", help="Apply quantization to the model for performance optimization.")
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device to use for inference.")
+    parser = argparse.ArgumentParser(
+        description="Generate structured academic paper reviews using a trained language model."
+    )
+    parser.add_argument(
+        "--pdf_file", type=str, required=True, help="Path to the PDF file."
+    )
+    parser.add_argument(
+        "--model_id",
+        type=str,
+        default="travis0103/mistral_7b_paper_review_lora",
+        help="Hugging Face model identifier.",
+    )
+    parser.add_argument(
+        "--quantize",
+        action="store_true",
+        help="Apply quantization to the model for performance optimization.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device to use for inference.",
+    )
 
     args = parser.parse_args()
     if "cuda" in args.device and not torch.cuda.is_available():
-        raise RuntimeError("CUDA is not available. Please switch to CPU or install a GPU.")
+        raise RuntimeError(
+            "CUDA is not available. Please switch to CPU or install a GPU."
+        )
     if args.device == "cpu" and args.quantize:
-        raise RuntimeError("Quantization is not supported on CPU. Please switch to a CUDA device.")
+        raise RuntimeError(
+            "Quantization is not supported on CPU. Please switch to a CUDA device."
+        )
 
     output_logger = setup_logger("output_logger", "logs/output.log")
     args = parser.parse_args()
@@ -113,16 +151,16 @@ if __name__ == "__main__":
     pdf = scipdf.parse_pdf_to_dict(args.pdf_file)
     content = parse_pdf_abstract(pdf)
     user_input = generate_user_input(content)
-    
+
     # Generate user input
     user_input = generate_user_input(content)
     output_logger.info(user_input)
     output_logger.info("=" * 50)
-    
+
     output_logger.info("Loading model...")
     model, tokenizer = load_model(args.model_id, args.quantize, args.device)
     output_logger.info("=" * 50)
-    
+
     # Generate the review
     output_logger.info("Generating review...")
     start_time = time.time()
@@ -130,6 +168,6 @@ if __name__ == "__main__":
     end_time = time.time()
     output_logger.info(f"Review generated in {end_time - start_time:.2f} seconds.")
     output_logger.info("=" * 50)
-    
+
     output_logger.info("Review:")
     output_logger.info(reviews)
